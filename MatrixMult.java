c53412169c2c3c25eb4578709797eca2003ba731
Snowsock <77976557+Snowsock@users.noreply.github.com>
2021-10-22 10:17:03 +0200

    Add files via upload

---
+++ MatrixMult.java
@@ -0,0 +1,51 @@
+
+package Task1;
+
+import java.util.*;
+
+public class MatrixMult {
+
+    public static void multiply(double[][] A, double[][] B, double[][] C) {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < A.length; i++) {
+            RowMultiplierTask task = new RowMultiplierTask(C, A, B, i);
+            Thread thread = new Thread(task);
+
+
+
+
+            // TODO: your solution here
+
+
+
+
+
+            if (threads.size() % 10 == 0) { waitForThreads(threads); }
+        }
+        if (!threads.isEmpty()) { waitForThreads(threads); }
+    }
+
+    private static void waitForThreads(List<Thread> threads) {
+        for (Thread thread : threads) {
+            try {
+
+
+
+
+
+                // TODO: your solution here
+
+
+
+
+                
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        threads.clear();
+    }
+}
+
+
